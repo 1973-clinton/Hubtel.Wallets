6ef45eaa8c641c0dd762230623adc24bdc5b585e
clinton <clinton.boamah@outlook.com>
2023-05-20 23:50:43 +0000

    added walllet model

---
+++ Hubtel.Wallets.Api/Models/Wallet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Hubtel.Wallets.Api.Models
+{
+    public class Wallet
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public string AccountNumber { get; set; }
+        public string AccountScheme { get; set; }
+        public string Owner { get; set; } 
+        public DateTime CreatedAt { get; set; }
+
+    }
+}
