9ab6717851bb988849d8c095a0533b78fa7d9fa6
clinton <clinton.boamah@outlook.com>
2023-05-22 14:04:45 +0000

    improved validation checks

---
+++ Hubtel.Wallets.Api/Constants/ValidationConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace Hubtel.Wallets.Api.Constants
+{
+    public static class ValidationConstants
+    {
+        // Regular Expressions
+        public const string MustBeAplhabetsOnly = @"^[a-zA-Z]";
+        public const string MustBeNumericOnly = @"^[0-9]";
+    }
+}
--- Hubtel.Wallets.Api/Validations/WalletValidator.cs
+++ Hubtel.Wallets.Api/Validations/WalletValidator.cs
@@ -1,31 +1,51 @@
 ﻿using FluentValidation;
+using Hubtel.Wallets.Api.Constants;
 using Hubtel.Wallets.Api.Dtos;
-using Hubtel.Wallets.Api.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Hubtel.Wallets.Api.Validations
 {
     public class WalletValidator : AbstractValidator<WalletDto>
     {
+
         public WalletValidator()
         {
-            RuleFor(wallet => wallet.Name).NotEmpty().MaximumLength(100).Matches(@"^[a-zA-Z]");
+            var walletTypes = new List<string>()
+            {
+                WalletTypeConstants.Card,
+                WalletTypeConstants.Momo
+            };
 
-            RuleFor(wallet => wallet.Owner).NotEmpty().MaximumLength(10).Matches(@"^[0-9]");
+            var accountSchemes = new List<string>()
+            {
+                AccountSchemeConstants.MasterCard,
+                AccountSchemeConstants.Mtn,
+                AccountSchemeConstants.Visa,
+                AccountSchemeConstants.Vodafone,
+                AccountSchemeConstants.AirtelTigo
+            };
 
-            RuleFor(wallet => wallet.AccountScheme).NotEmpty();
+            RuleFor(wallet => wallet.Name).NotEmpty().MaximumLength(100).Matches(ValidationConstants.MustBeAplhabetsOnly);
 
-            RuleFor(wallet => wallet.Type).NotEmpty().MaximumLength(4);
+            RuleFor(wallet => wallet.Owner).NotEmpty().MaximumLength(10).Matches(ValidationConstants.MustBeNumericOnly);
 
+            RuleFor(wallet => wallet.AccountScheme).NotEmpty().Must(p => accountSchemes.Contains(p));
+
+            RuleFor(wallet => wallet.Type).NotEmpty().MaximumLength(4).Must(p => walletTypes.Contains(p));
+            
             // validates account number if it starts with phone number code '0', else it validates it as credit card
             When(wallet => wallet.AccountNumber.StartsWith("0"), () =>
             {
-                RuleFor(wallet => wallet.AccountNumber).NotEmpty().MaximumLength(10).Matches(@"^[0-9]");               
+                RuleFor(wallet => wallet.AccountNumber).NotEmpty().MaximumLength(10).Matches(ValidationConstants.MustBeNumericOnly);               
             })
             .Otherwise(() =>
             {
-                RuleFor(wallet => wallet.AccountNumber).NotEmpty().CreditCard().Matches(@"^[0-9]");
+                RuleFor(wallet => wallet.AccountNumber).NotEmpty().CreditCard().Matches(ValidationConstants.MustBeNumericOnly);
             });
 
         }
+
+        
     }
 }
